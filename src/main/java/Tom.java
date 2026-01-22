b0c9f0f5fe6bd1b1e7a3f4659c0579c891f19dc7
Damian <damianeng@damians-MacBook-Air-3.local>
2026-01-22 14:28:49 +0800

    Completed Level-0: renamed bot

---
+++ src/main/java/Tom.java
@@ -0,0 +1,14 @@
+public class Tom {
+    public static void main(String[] args) {
+        String greeting = "Hello! I'm Tom! \n"
+                        + "What can I do for you?";
+        String border = "____________________________________________________________";
+        String exit = "Bye. Hope to see you again soon!";
+        System.out.println(border);
+        System.out.println(greeting);
+        System.out.println(border);
+        System.out.println(exit);
+        System.out.println(border);
+
+    }
+}
