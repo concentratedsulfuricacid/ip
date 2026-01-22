5f8665461f25808de707c0bbe41cccffd4f133a4
Damian <damianeng@damians-MacBook-Air-3.local>
2026-01-22 14:52:34 +0800

    completed Level-2: Add, List

--- src/main/java/Tom.java
+++ src/main/java/Tom.java
@@ -8,6 +8,30 @@ public class Tom {
         System.out.println(input);
         System.out.println(border);
     }
+    static String[] items = new String[100];
+
+    public static void add(String items) {
+        for (int i = 0; i < Tom.items.length; i++) {
+            if (Tom.items[i] == null) {
+                Tom.items[i] = items;
+
+                System.out.println(border);
+                System.out.println("added: " + items);
+                System.out.println(border);
+                break;
+            }
+        }
+    }
+
+    public static void list_items() {
+        System.out.println(border);
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] != null) {
+                System.out.println((i + 1) + ". " + items[i]);
+            }
+        }
+        System.out.println(border);
+    } 
     
     public static void main(String[] args) {
         String greeting = "Hello! I'm Tom! \n"
@@ -21,7 +45,11 @@ public class Tom {
 
         String message = scanner.nextLine();
         while (!message.equals("bye")) {
-            echo(message);
+            if (message.equals("list")) {
+                list_items();
+            } else {
+                add(message);
+            }
             message = scanner.nextLine();
         }
         
