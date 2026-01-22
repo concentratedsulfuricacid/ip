a94f89be7dd2f93a9fef8ba75f4abeaf783ce2e5
Damian <damianeng@damians-MacBook-Air-3.local>
2026-01-22 14:42:42 +0800

    completed Level-1: Echo

--- src/main/java/Tom.java
+++ src/main/java/Tom.java
@@ -1,11 +1,30 @@
+import java.util.Scanner;
 public class Tom {
+    static Scanner scanner = new Scanner(System.in);
+    static String border = "____________________________________________________________";
+    
+    public static void echo(String input){
+        System.out.println(border);
+        System.out.println(input);
+        System.out.println(border);
+    }
+    
     public static void main(String[] args) {
         String greeting = "Hello! I'm Tom! \n"
                         + "What can I do for you?";
-        String border = "____________________________________________________________";
         String exit = "Bye. Hope to see you again soon!";
+
+        
         System.out.println(border);
         System.out.println(greeting);
+        System.out.println(border);
+
+        String message = scanner.nextLine();
+        while (!message.equals("bye")) {
+            echo(message);
+            message = scanner.nextLine();
+        }
+        
         System.out.println(border);
         System.out.println(exit);
         System.out.println(border);
